2c1f51a17f2f4f198982e3e641cf8fe870cb17b5
gustavo-kzqs <gustavo.kazeker@gmail.com>
2025-02-11 11:43:34 -0300

    Configuração do ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Teste from './Teste'
+import EstiloGlobal from './Styles'
+
+function App() {
+  return (
+    <>
+  <Teste />
+  <EstiloGlobal />
+    </>
+  )
+
+}
+
+export default App;
---
+++ src/Teste.tsx
@@ -0,0 +1,16 @@
+import styled  from "styled-components";
+
+
+const Botao = styled.button `
+  background-color: red;
+`
+
+function Teste() {
+  return (
+    <>
+  <Botao>Clique aqui</Botao>
+    </>
+  )
+}
+
+export default Teste
